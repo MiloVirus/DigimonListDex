1237c6277c968bca60008e19c8ec0b1dd3c3b6d5
MilozVirus <cami_alejo6@hotmail.com>
2022-09-02 19:47:34 -0500

    Seventh Commit

--- js/main.js
+++ js/main.js
@@ -4,19 +4,20 @@ const callApi = async() =>
     const response = await axios.get ("https://digimon-api.vercel.app/api/digimon")
     if(response)
     {
-        console.log(response.data)
+        //console.log(response.data)
         
 
         
         response.data.forEach(element => {
 
-            let digiName= element.name          
-            console.log(digiName)
+            let digiName = element.name
+            let digimon = element          
+      
            let list = document.createElement("div")
-           list.setAttribute("class","col-4 colDigi")
+           list.setAttribute("class","col-2 colDigi")
            list.innerHTML= `
            
-                <div class="row rowDigi" id="${digiName}" onclick = callDigimon(${JSON.stringify(element)})>
+                <div class="row rowDigi" id="${digiName}" onclick='callDigimon(${JSON.stringify(digimon)})'>
                     <div class="col-8">
                         <div class="digiName">${element.name}</div>
                         <div class="digiLevel">${element.level}</div>
@@ -25,8 +26,6 @@ const callApi = async() =>
                 </div>
            `
            document.getElementById("listDigimon").appendChild(list) 
-           console.log(element)
-           
         });
     } 
 }
@@ -34,7 +33,6 @@ const callApi = async() =>
 const callDigimon = async(clicked_id) => 
         {
             let digimon = (clicked_id.name)
-            console.log(digimon)
             const responseDigi = await axios.get(`https://digimon-api.vercel.app/api/digimon/name/${digimon}`)
             if(responseDigi)
             {
@@ -42,61 +40,56 @@ const callDigimon = async(clicked_id) =>
                 if(response)
                 {
                     const array = response.data
-
-                
-                        const result = array.filter(array => array.level === clicked_id.level)
-                        const fresh = array.filter(array => array.level === "Fresh")
-                        const training = array.filter(array => array.level === "In Training")
-                        const rookie = array.filter(array => array.level === "Rookie")
-                        const champion = array.filter(array => array.level === "Champion")
-                        const ultimate = array.filter(array => array.level === "Ultimate")
-                        const mega = array.filter(array => array.level === "Mega")
+              
+                        const {result, fresh, intraining, rookie, champion, ultimate, mega, training, armor} = 
+                        {
+                          result : array.filter(array => array.level === clicked_id.level),
+                          fresh : array.filter(array => array.level === "Fresh"),
+                          intraining : array.filter(array => array.level === "In Training"),
+                          rookie : array.filter(array => array.level === "Rookie"),
+                          champion : array.filter(array => array.level === "Champion"),
+                          ultimate : array.filter(array => array.level === "Ultimate"),
+                          mega : array.filter(array => array.level === "Mega"),
+                          training : array.filter(array => array.level === "Training"),
+                          armor : array.filter(array => array.level === "Armor")
+                         }
                         
-                        const different = array.filter(array => array.level !== clicked_id.level)
-                        
-                        console.log(fresh)
-                        console.log(training)
-                        console.log(rookie)
-                        console.log(champion)
-                        console.log(ultimate)
-                        console.log(mega)
-                        
-                    
-
-                    const labelsDigi = [
-                        fresh[1].level,
-                        training[1].level,
-                        rookie[1].level,
-                        champion[1].level,
-                        ultimate[1].level,
-                        mega[1].level,
-                      ];
-
+                        unique = [...new Set(array.map(a => a.level))]
+                   
                       const labelsDigiData = [
-                        fresh.length,
-                        training.length,
-                        rookie.length,
-                        champion.length,
-                        ultimate.length,
-                        mega.length,
+                        {id : 0, level: "In Training" , lengthDigi: intraining.length},
+                        {id : 1, level: "Rookie" , lengthDigi: rookie.length }, 
+                        {id : 2, level: "Champion" , lengthDigi: champion.length},
+                        {id : 3, level: "Ultimate" , lengthDigi: ultimate.length},
+                        {id : 4, level: "Fresh" , lengthDigi: fresh.length},
+                        {id : 5, level: "Mega" , lengthDigi: mega.length},
+                        {id : 6, level: "Training" , lengthDigi: training.length},
+                        {id : 7, level: "Armor" , lengthDigi: armor.length}
                       ];
-                    
-                    
-                    const newArray = labelsDigi.filter(labelsDigi => labelsDigi !== clicked_id.level)
+                     
+                    let newArrayData = []
+                      
+                    labelsDigiData.forEach(element=>{
+
+                      newArrayData = labelsDigiData.filter(element => element.level !== clicked_id.level)
+    
+                    })
+                    newArrayData.unshift({id: 8, level: clicked_id.level, lengthDigi: result.length})
+                    console.log(newArrayData)
+
+                    let newArray = unique.filter(unique => unique !== clicked_id.level)
                     newArray.unshift(clicked_id.level)
                     
-                    const newArrayData = labelsDigiData.filter(labelsDigiData => labelsDigiData !== result.length)
-                    newArrayData.unshift(result.length)
-                    console.log(newArrayData)
+                    newArrayData = newArrayData.map(element=>element.lengthDigi)
+                    
                     
                 
 
-                const labels = newArray;
-                
+                const labels = newArray
                   const dataChart = {
                     labels: labels,
                     datasets: [{
-                      label: 'My First dataset',
+                      label: 'Digimon Level Distribution',
                       backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(255, 159, 64, 0.2)',
@@ -133,13 +126,16 @@ const callDigimon = async(clicked_id) =>
                 let newDigimon = document.getElementById("digimonContainer")
                 
                 newDigimon.innerHTML = `
-                    <div class = "col-md-12"><h3>${responseDigi.data[0].name}</h3></div> 
-                    <div class = "col-md-12"><img src="${responseDigi.data[0].img}"></div>
-                    <div class = "col-md-12"><h4>${responseDigi.data[0].level}</h4></div>
-                    <div>
+                    <div class="row" id="containerDigiList">
+                      <div class="col">
+                        <div class = "col-md-12"><h3>${responseDigi.data[0].name}</h3></div> 
+                        <div class = "col-md-12"><img src="${responseDigi.data[0].img}"></div>
+                        <div class = "col-md-12"><h4>${responseDigi.data[0].level}</h4></div>
+                      </div> 
+                      <div class="col">
                         <canvas id="myChart"></canvas>
+                      </div> 
                     </div>
-
                 `  
                 const myChart = new Chart(
                     document.getElementById('myChart'),
